c55314908068f05234426e805fff55aa94a1228c
Carlos Salvatierra <cslucano@gmail.com>
2014-08-24 17:13:26 -0500

    extraemos todas las columnas del padron

--- extract_padron.rb
+++ extract_padron.rb
@@ -5,14 +5,21 @@ files = Dir.glob "./input/*.pdf"
 outfilename = "./output/padron_electoral.csv"
 out = open(outfilename, 'w')
 
-area_input =  [130, 30, 1300, 180]
+areas =  [
+             [130,  30, 800, 210],
+             [130, 210, 800, 390],
+             [130, 390, 800, 570]
+         ]
 files.each do |file_name|
     print file_name
-    extractor = Tabula::Extraction::ObjectExtractor.new(file_name, :all)
+    extractor = Tabula::Extraction::ObjectExtractor.new(file_name, [1])
     extractor.extract.each do |pdf_page|
-      page_area = pdf_page.get_area(area_input)
-      table = page_area.make_table({})
-      Tabula::Writers.CSV(table, out)
+      page_areas = areas.map{ |area| pdf_page.get_area(area) }
+      tables = page_areas.map{ |page_area| page_area.make_table({}) }
+
+      tables.each do |table| 
+        Tabula::Writers.CSV(table, out)
+      end
     end
 end
 
